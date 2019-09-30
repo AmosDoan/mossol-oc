0584be42635aea14def484ff3f8d8106cf6e3dac
AmosDoan <amos.doan@gmail.com>
2019-10-01 00:28:41 +0900

    - First commit

---
+++ web/src/test/java/net/mossol/oc/OcApplicationTests.java
@@ -0,0 +1,16 @@
+package net.mossol.oc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
