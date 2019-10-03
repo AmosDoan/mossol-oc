8d86f0e66f246a61179820e68243df1f6a04eebd
amos_doan <amos.doan@gmail.com>
2019-10-03 14:50:39 +0900

    - Rename web to backend

--- backend/src/main/java/net/mossol/oc/OcApplication.java
+++ backend/src/main/java/net/mossol/oc/OcApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OcApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(OcApplication.class, args);
 	}
 
