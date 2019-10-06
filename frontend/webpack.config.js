8feb2f9353084dbeeb3e0a0cd745fd85709d75e9
amos_doan <amos.doan@gmail.com>
2019-10-07 00:08:38 +0900

    Working

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,14 +1,31 @@
 module.exports = {
     devtool: 'source-map',
     module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-        }]
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use : {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/react']
+                    }
+                }
+            }, 
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
     },
     resolve: {
         extensions: ['.js', '.jsx']
